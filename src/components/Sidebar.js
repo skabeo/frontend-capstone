0ebf17395ac2a0ffcbbd8b4b77029adeeb4c782a
skabeo <spencerokyere95@gmail.com>
2023-08-21 22:41:40 +0000

    Fix bugs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,7 +16,8 @@ const Sidebar = () => {
         <Link to="/reserve">Reserve property</Link>
         <Link to="/reservations">My Reservations</Link>
         <Link to="/add-property">Add property</Link>
-        <Link to="/delete">Delete</Link>
+        <Link to="/delete">Delete property</Link>
+        <Link to='/signout'>Logout</Link>
       </div>
     );
   } else if (!accessToken && !loading) {
