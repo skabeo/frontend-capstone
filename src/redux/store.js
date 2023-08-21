3f14c61add9769a10c5e18b8576912bc8439cf27
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-21 17:02:54 +0300

    Update store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from './sessions/sessionSlice';
 import propertiesReducer from './properties/propertiesSlice';
+import reservationReducer from './reservations/reserveSlice';
 
 const store = configureStore({
   reducer: {
     properties: propertiesReducer,
     session: sessionReducer,
+    reservations: reservationReducer
   },
 });
 
