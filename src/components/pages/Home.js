6acdcf845fcc436f25d697c08d9fbe18d4fd577f
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-21 13:10:11 +0300

    Create and update onClick function

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,20 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPortfolio } from '../../redux/properties/propertiesSlice';
 import { PropertySquare } from '../PropertySquare';
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
   const portfolio = useSelector((state) => state.properties.portfolio);
   const dispatch = useDispatch();
+  const nav = useNavigate();
 
   useEffect(() => {
     dispatch(fetchPortfolio());
   }, [dispatch]);
+
+  const switchPage = (property) => {
+    nav(`property/${property.id}`, {state: {property}})
+  }
 
   if (portfolio) {
     return (
@@ -25,6 +31,7 @@ export const Home = () => {
                 price={property.price}
                 location={property.location}
                 image={property.image}
+                onClick={() => switchPage(property)}
               />
             ))
               : <p>Nothing to show</p>}
