e63f4b35b56a3e69d90d7901cccdb88c3e0e84e3
adeola003 <khadaf3@gmail.com>
2023-08-22 11:34:51 +0100

    display all properties in delete component

--- src/components/pages/Delete.js
+++ src/components/pages/Delete.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchPortfolio } from '../../redux/properties/propertiesSlice';
+import { PropertySquare } from '../PropertySquare';
 
-const Delete = () => (
-  <div>Delete</div>
-);
+export const Delete = () => {
+  const portfolio = useSelector((state) => state.properties.portfolio);
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+
+  useEffect(() => {
+    dispatch(fetchPortfolio());
+  }, [dispatch]);
+
+  const switchPage = (property) => {
+    nav(`property/${property.id}`, { state: { property } });
+  };
+
+  if (portfolio) {
+    return (
+      <div>
+        <div>
+          <h1>Home Page</h1>
+          <p>Welcome to Get more properties</p>
+          <div className="homePagePropertiesContainer">
+            {portfolio.length ? portfolio.map((property) => (
+              <PropertySquare
+                key={property.id}
+                name={property.name}
+                price={property.price}
+                location={property.location}
+                image={property.image}
+                onClick={() => switchPage(property)}
+              />
+            ))
+              : <p>Nothing to show</p>}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div>
+        <h1>Home Page</h1>
+        <p>Welcome to Get more properties</p>
+      </div>
+    </div>
+  );
+};
 
 export default Delete;
