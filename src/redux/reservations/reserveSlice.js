16ee63d2e0908387a0bbb1f88666c22172ee13a0
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-23 11:45:46 +0300

    Update get function in the slice

--- src/redux/reservations/reserveSlice.js
+++ src/redux/reservations/reserveSlice.js
@@ -1,18 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:3000/api/v1' 
+
 const initialState = {
-  loading: true,
-  reservations: undefined,
+  isLoading: true,
+  reservations: [],
   error: '',
 };
+
+export const fetchReservations = createAsyncThunk(
+  'fetch/reservations',
+  async (accessToken) => {
+    try {
+      const response = await axios.get('http://127.0.0.1:3000/api/v1/reservations', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+);
 
 export const createReservation = (accessToken, data) => async () => {
   const { property, date, city } = data;
   const newReservation = { property_id: property.id, city, date };
 
   const response = await axios.post(
-    'http://127.0.0.1:3000/api/v1/reservations',
+    `${BASE_URL}/reservations`,
     newReservation,
     {
       headers: {
@@ -26,7 +45,20 @@ export const createReservation = (accessToken, data) => async () => {
 const reservationSlice = createSlice({
   name: 'reservation',
   initialState,
-  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchReservations.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchReservations.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.reservations = action.payload.reservation;
+    });
+    builder.addCase(fetchReservations.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+      console.log(action.payload);
+    });
+  },
 });
 
 export default reservationSlice.reducer;
