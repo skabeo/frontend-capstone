8d1170414ca92647fffc096c03155ba3c0094e34
skabeo <spencerokyere95@gmail.com>
2023-08-23 09:24:14 +0000

    Fix checker clause

--- src/components/pages/Reservations.js
+++ src/components/pages/Reservations.js
@@ -3,38 +3,32 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchReservations } from '../../redux/reservations/reserveSlice';
 
 const Reservations = () => {
-  const reservation = useSelector((state) => state.reservations.reservations);
+  const reservations = useSelector(state => state.reservations.reservations);
+  const accessToken = useSelector(state => state.session.accessToken);
+  const currentUser = useSelector(state => state.session.currentUser);
+
   const dispatch = useDispatch();
-  const accessToken = useSelector((state) => state.session.accessToken);
-  const currentUser = useSelector((state) => state.session.currentUser);
 
   useEffect(() => {
     dispatch(fetchReservations(accessToken));
-  }, [dispatch]);
-
-  if (reservation) {
-    return (
-      <div>
-        <div>
-          <h1>My Reservations</h1>
-          <div className="homePagePropertiesContainer">
-            {reservation.length ? reservation.map((reserve) => (
-              <div key={reserve.id}>
-                <h3>{currentUser.name}</h3>
-                <p>{reserve.date_of_visit}</p>
-                <p>{reserve.city}</p>
-              </div>
-            ))
-              : <p>Nothing to show</p>}
-          </div>
-        </div>
-      </div>
-    );
-  }
+  }, [dispatch, accessToken]);
 
   return (
     <div>
-      <h2 className="home-title">My Reservations</h2>
+      <h1>My Reservations</h1>
+      <div className="homePagePropertiesContainer">
+        {reservations.length ? (
+          reservations.map(reserve => (
+            <div key={reserve.id}>
+              <h3>{currentUser.name}</h3>
+              <p>Date of Visit: {reserve.date_of_visit}</p>
+              <p>City: {reserve.city}</p>
+            </div>
+          ))
+        ) : (
+          <p>Nothing to show</p>
+        )}
+      </div>
     </div>
   );
 };
