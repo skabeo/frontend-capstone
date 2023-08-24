9518b11dbd59ce662a89159709421a1f9e19c54f
adeola003 <khadaf3@gmail.com>
2023-08-24 10:35:33 +0100

    add one test case for delete component

---
+++ src/test/delete.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import store from '../redux/store';
+import Delete from '../components/pages/Delete';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Delete page', () => {
+  test('Render list of properties with delte buttons', () => {
+    render(
+      <Router>
+        <Provider store={store}>
+          <Delete />
+        </Provider>
+      </Router>,
+    );
+    const pageTitle = screen.getByText('Remove properties');
+    expect(pageTitle).toBeInTheDocument();
+  });
+});
