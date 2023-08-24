dca866f485258d589d70477349e711bd18e39fae
adeola003 <khadaf3@gmail.com>
2023-08-24 20:10:51 +0100

    fix eslint

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,22 +10,22 @@ const Sidebar = () => {
   if (accessToken) {
     sessionLinks = (
       <>
-      <img className='app-logo' src="https://source.unsplash.com/ZsuqlSxrjRk" alt="Application logo" />
-      <div className='app-links'>
-        <Link to="/">Properties</Link>
-        <Link to="/reserve">Reserve property</Link>
-        <Link to="/reservations">My Reservations</Link>
-        <Link to="/add-property">Add property</Link>
-        <Link to="/delete">Delete property</Link>
-      </div>
-      <div className='session-btn'><Link to="/signout">Logout</Link></div>
+        <img className="app-logo" src="https://source.unsplash.com/ZsuqlSxrjRk" alt="Application logo" />
+        <div className="app-links">
+          <Link to="/">Properties</Link>
+          <Link to="/reserve">Reserve property</Link>
+          <Link to="/reservations">My Reservations</Link>
+          <Link to="/add-property">Add property</Link>
+          <Link to="/delete">Delete property</Link>
+        </div>
+        <div className="session-btn"><Link to="/signout">Logout</Link></div>
       </>
     );
   }
 
   return (
     <div className="sidebar large-screens">
-        {sessionLinks}
+      {sessionLinks}
     </div>
   );
 };
