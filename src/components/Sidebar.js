a52b62638352dff2c25f37c490a70505b81c0c55
adeola003 <khadaf3@gmail.com>
2023-08-24 20:47:52 +0100

    change sidebar component extension

---
+++ src/components/Sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import '../styles/sidebar.css';
+
+const Sidebar = () => {
+  const accessToken = useSelector((state) => state.session.accessToken);
+
+  let sessionLinks;
+  if (accessToken) {
+    sessionLinks = (
+      <>
+        <img className="app-logo large-screens" src="https://source.unsplash.com/ZsuqlSxrjRk" alt="Application logo" />
+        <div className="app-links large-screens">
+          <NavLink to="/" activeClassName="active-link">Properties</NavLink>
+          <NavLink to="/reserve" activeClassName="active-link">Reserve property</NavLink>
+          <NavLink to="/reservations" activeClassName="active-link">My Reservations</NavLink>
+          <NavLink to="/add-property" activeClassName="active-link">Add property</NavLink>
+          <NavLink to="/delete" activeClassName="active-link">Delete property</NavLink>
+        </div>
+        <div className="session-btn large-screens"><Link to="/signout">Logout</Link></div>
+      </>
+    );
+  }
+
+  return (
+    <div className="sidebar large-screens">
+      {sessionLinks}
+    </div>
+  );
+};
+
+export default Sidebar;
