3354f838c8b56a0ad6d3493766590b781f9bf9a4
adeola003 <khadaf3@gmail.com>
2023-08-24 11:18:42 +0100

    remove mock store

--- src/test/delete.test.js
+++ src/test/delete.test.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
 import store from '../redux/store';
-import { configureStore } from '@reduxjs/toolkit';
 import Delete from '../components/pages/Delete';
 import '@testing-library/jest-dom/extend-expect';
-import { ReducerWithInitialState } from '@reduxjs/toolkit/dist/createReducer';
 
 describe('Delete page', () => {
   test('Renders the app when there is nothing to show', () => {
@@ -24,46 +22,4 @@ describe('Delete page', () => {
     expect(displaymsg).toBeInTheDocument();
   });
 
-  test('Render list of properties with delete buttons', () => {
-    const initialState = {
-        portfolio: [
-          {
-            id: 1,
-            name: 'Royal palace',
-            price: 100,
-            location: 'Cotonou',
-            image: 'image1.jpg',
-          },
-          {
-            id: 2,
-            name: 'Aafin Awujale',
-            price: 200,
-            location: 'Ijebu Ode',
-            image: 'image2.jpg',
-          },
-        ],
-      };
-
-      const reducer = ReducerWithInitialState(initialState);
-
-
-
-      // Mock Redux state to have the portfolio with properties
-      render(
-        <Router>
-          <Provider store={store}>
-            <Delete />
-          </Provider>
-        </Router>,
-      );
-
-      // Check if properties are rendered
-      const Delete = screen.getByText('Delete');;
-      expect(Delete).toBeInTheDocument();
-
-      // Simulate clicking the delete button for the first property
-    //   const deleteButton = screen.getAllByText('Delete')[0];
-    //   fireEvent.click(deleteButton);
-
-  });
 });
