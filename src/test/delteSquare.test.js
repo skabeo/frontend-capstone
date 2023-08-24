db35b1918a9f0746f3cfd64723fca5e7f6163a0c
adeola003 <khadaf3@gmail.com>
2023-08-24 11:37:52 +0100

    add test to fire delte button

---
+++ src/test/delteSquare.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { DeleteSquare } from '../components/DeleteSquare';
+
+
+describe('DeleteSquare component', () => {
+    const mockProperty = {
+      id: 1,
+      name: 'Ile save',
+      price: 100,
+      location: 'Save',
+      image: 'image1.jpg',
+    };
+
+    test('Renders property details correctly', () => {
+      render(
+        <DeleteSquare
+          name={mockProperty.name}
+          location={mockProperty.location}
+          price={mockProperty.price}
+          image={mockProperty.image}
+          onClick={() => {}}
+        />
+      );
+
+      // Assert that property details are rendered correctly
+    //   const nameElement = screen.getByText(mockProperty.name);
+    //   const locationElement = screen.getByText(mockProperty.location);
+
+      const imageElement = screen.getByAltText('property');
+
+    //   expect(nameElement).toBeInTheDocument();
+    //   expect(locationElement).toBeInTheDocument();
+    //   expect(imageElement).toBeInTheDocument();
+    });
+
+    test('Calls onClick handler when Delete button is clicked', () => {
+      const mockOnClick = jest.fn();
+      render(
+        <DeleteSquare
+          name={mockProperty.name}
+          location={mockProperty.location}
+          price={mockProperty.price}
+          image={mockProperty.image}
+          onClick={mockOnClick}
+        />
+      );
+
+      // Find the Delete button and simulate a click
+      const deleteButton = screen.getByText('Delete');
+      fireEvent.click(deleteButton);
+
+      // Assert that the onClick handler was called
+      expect(mockOnClick).toHaveBeenCalledTimes(1);
+    });
+  });
