de55130e6f49af3beb541c8919f60fc6c2c1488b
adeola003 <khadaf3@gmail.com>
2023-08-24 21:34:31 +0100

    fix eslint

--- src/components/pages/Delete.js
+++ src/components/pages/Delete.js
@@ -19,30 +19,30 @@ export const Delete = () => {
   if (portfolio) {
     return (
       <div>
-          <h1 className='delPage-title'>Remove properties</h1>
-          <p className='delPage-msg'>Welcome to the page where you can navigate through the properties and remove them</p>
-          <div className="delPagePropertiesContainer">
-            {portfolio.length ? portfolio.map((property) => (
-              <DeleteSquare
-                key={property.id}
-                name={property.name}
-                price={property.price}
-                location={property.location}
-                image={property.image}
-                onClick={() => handleDelete(property.id)}
-              />
-            ))
-              : <p className='delPage-msg'>Nothing to show</p>}
-          </div>
+        <h1 className="delPage-title">Remove properties</h1>
+        <p className="delPage-msg">Welcome to the page where you can navigate through the properties and remove them</p>
+        <div className="delPagePropertiesContainer">
+          {portfolio.length ? portfolio.map((property) => (
+            <DeleteSquare
+              key={property.id}
+              name={property.name}
+              price={property.price}
+              location={property.location}
+              image={property.image}
+              onClick={() => handleDelete(property.id)}
+            />
+          ))
+            : <p className="delPage-msg">Nothing to show</p>}
+        </div>
       </div>
     );
   }
   return (
     <div>
       <div>
-        <h1 className='delPage-title'>Remove properties</h1>
-        <p className='delPage-msg'>Welcome to the page where you can navigate through the properties and remove them</p>
-        <p className='delPage-msg'>Nothing here for now! Check the App property link to add new properties</p>
+        <h1 className="delPage-title">Remove properties</h1>
+        <p className="delPage-msg">Welcome to the page where you can navigate through the properties and remove them</p>
+        <p className="delPage-msg">Nothing here for now! Check the App property link to add new properties</p>
       </div>
     </div>
   );
