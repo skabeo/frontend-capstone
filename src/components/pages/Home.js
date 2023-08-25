826f63f10f2d3ae084fc4112f878d2f83a535bbf
Juan Diaz <jfdiaza@unal.edu.co>
2023-08-25 13:57:16 -0400

    create responsive const

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,6 +8,24 @@ import { useNavigate } from 'react-router-dom';
 import { fetchPortfolio } from '../../redux/properties/propertiesSlice';
 import { PropertySquare } from '../PropertySquare';
 
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+
 export const Home = () => {
   const portfolio = useSelector((state) => state.properties.portfolio);
   const dispatch = useDispatch();
