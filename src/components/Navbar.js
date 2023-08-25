97d7b28ee98961e8829eb480cd8b61253d8b1137
skabeo <spencerokyere95@gmail.com>
2023-08-25 11:59:43 +0000

    Fix minor nav and sidebar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import menu from '../assets/hamburger.png';
+import close from '../assets/close.png';
 import '../styles/navbar.css';
 
 const Navbar = () => {
@@ -26,13 +28,13 @@ const Navbar = () => {
     return (
       <div className={`nav-container small-screen ${isMenuOpen ? 'menu-open' : ''}`}>
         <div className="nav-elements">
+          <span>get more</span>
           <button type="button" className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-            <span className="bar" />
-            <span className="bar" />
-            <span className="bar" />
+            <img src={isMenuOpen ? close : menu} alt="hamburger-meny" className="navbar-open" />
           </button>
           {sessionLinks}
         </div>
+        <hr className="navbar-line" />
       </div>
     );
   }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,18 +5,29 @@ import '../styles/sidebar.css';
 
 const Sidebar = () => {
   const accessToken = useSelector((state) => state.session.accessToken);
+  const { name } = useSelector((state) => state.session.currentUser);
 
   let sessionLinks;
   if (accessToken) {
     sessionLinks = (
       <>
-        <img className="app-logo large-screens" src="https://source.unsplash.com/ZsuqlSxrjRk" alt="Application logo" />
-        <div className="app-links large-screens">
-          <NavLink to="/" activeClassName="active-link">Properties</NavLink>
-          <NavLink to="/reserve" activeClassName="active-link">Reserve property</NavLink>
-          <NavLink to="/reservations" activeClassName="active-link">My Reservations</NavLink>
-          <NavLink to="/add-property" activeClassName="active-link">Add property</NavLink>
-          <NavLink to="/delete" activeClassName="active-link">Delete property</NavLink>
+        <div className="center-text">
+          <p className="bold">get more</p>
+          <p className="bold">properties</p>
+          <hr className="sidebar-logo-line" />
+          <p className="sidebar-username">
+            Welcome
+            <span className="sidebar-name">{name}</span>
+          </p>
+        </div>
+        <div className="sidebar-links-container">
+          <div className="app-links large-screens">
+            <NavLink to="/" activeClassName="active-link">Properties</NavLink>
+            <NavLink to="/reserve" activeClassName="active-link">Reserve property</NavLink>
+            <NavLink to="/reservations" activeClassName="active-link">My Reservations</NavLink>
+            <NavLink to="/add-property" activeClassName="active-link">Add property</NavLink>
+            <NavLink to="/delete" activeClassName="active-link">Delete property</NavLink>
+          </div>
         </div>
         <Link to="/signout" className="session-btn large-screens">Logout</Link>
       </>
