c326274c97c7aa4fc63a69a42e4e5ed9c628f7e8
Juan Diaz <jfdiaza@unal.edu.co>
2023-08-25 13:59:11 -0400

    delete old content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -39,37 +39,7 @@ export const Home = () => {
     nav(`property/${property.id}`, { state: { property } });
   };
 
-  if (portfolio) {
-    return (
-      <div>
-        <div>
-          <h1>Home Page</h1>
-          <p>Welcome to Get more properties</p>
-          <div className="homePagePropertiesContainer">
-            {portfolio.length ? portfolio.map((property) => (
-              <PropertySquare
-                key={property.id}
-                name={property.name}
-                price={property.price}
-                location={property.location}
-                image={property.image}
-                onClick={() => switchPage(property)}
-              />
-            ))
-              : <p>Nothing to show</p>}
-          </div>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <div>
-        <h1>Home Page</h1>
-        <p>Welcome to Get more properties</p>
-      </div>
-    </div>
-  );
+ 
 };
 
 export default Home;
